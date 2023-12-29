7d65bef60d4e84583b65d3fda6a67c6d9d80b21d
JHFeng <lingj.jhf@outlook.com>
2023-12-29 15:15:57 +0800

    移除button

--- projects/sub-project1/pages/index.vue
+++ projects/sub-project1/pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <nt-button />
   <div>{{ hello() }}</div>
 </template>
 
 <script setup lang="ts">
-import { NtButton } from '@nuxt-template/components'
 import { hello } from '@nuxt-template/utils'
 </script>
 
