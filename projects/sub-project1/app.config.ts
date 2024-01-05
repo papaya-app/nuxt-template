83cad8bdfb924a87625359c3decc9e91cd50858f
JHFeng <lingj.jhf@outlook.com>
2024-01-05 15:54:04 +0800

    添加配置nuxtui主题示例

---
+++ projects/sub-project1/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  ui: {
+    button: {
+      default: {
+        color: 'yellow'
+      },
+      color: 'blue'
+    },
+    table: {
+      td: {
+        color: 'text-blue-500 dark:text-blue-400',
+      },
+    }
+  }
+})
--- projects/sub-project1/pages/index.vue
+++ projects/sub-project1/pages/index.vue
@@ -1,9 +1,49 @@
 <template>
   <div>{{ hello() }}</div>
+  <UButton>Hello</UButton>
+  <UTable :rows='people' />
 </template>
 
 <script setup lang="ts">
 import { hello } from '@nuxt-template/utils'
+
+const people = [{
+  id: 1,
+  name: 'Lindsay Walton',
+  title: 'Front-end Developer',
+  email: 'lindsay.walton@example.com',
+  role: 'Member'
+}, {
+  id: 2,
+  name: 'Courtney Henry',
+  title: 'Designer',
+  email: 'courtney.henry@example.com',
+  role: 'Admin'
+}, {
+  id: 3,
+  name: 'Tom Cook',
+  title: 'Director of Product',
+  email: 'tom.cook@example.com',
+  role: 'Member'
+}, {
+  id: 4,
+  name: 'Whitney Francis',
+  title: 'Copywriter',
+  email: 'whitney.francis@example.com',
+  role: 'Admin'
+}, {
+  id: 5,
+  name: 'Leonard Krasner',
+  title: 'Senior Designer',
+  email: 'leonard.krasner@example.com',
+  role: 'Owner'
+}, {
+  id: 6,
+  name: 'Floyd Miles',
+  title: 'Principal Designer',
+  email: 'floyd.miles@example.com',
+  role: 'Member'
+}]
 </script>
 
 <style scoped></style>
